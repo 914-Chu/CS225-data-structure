b653c3d52840f1548709fff1b2271707d3fd8517
Joyce Chu <chiaiic2@linux-a2.ews.illinois.edu>
2020-01-23 10:21:15 -0600

    Construct HSLAPixel .h and .cpp files according to instructions.

--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,20 @@
 #include <sstream>
 
 namespace cs225 {
+
+class HSLAPixel {
+
+    public:
+
+        double h;  //hue           [0,360)
+	double s;  //saturation    [0,1]
+	double l;  //luminance     [0,1]
+	dobule a;  //alpha channel [0,1]		
+	
+	HSLAPixel();
+	HSLAPixel(double hue, double saturation, double luminance);
+	HSLAPixel(double hue, double saturation, double luminance, double alpha);
+	
+};
 }
+
