a8db03a5fd13b785742c635bbc418cff4ce6bbdc
joycechu <joycechu@DESKTOP-1IDM1H6.localdomain>
2020-02-04 18:59:49 -0600

    update Makefile

--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -7,4 +7,16 @@
 #pragma once
 
 namespace cs225 {
+class HSLAPixel {
+	public:
+		double h;  //hue           [0,360)
+	    	double s;  //saturation    [0,1]
+		double l;  //luminance     [0,1]
+		double a;  //alpha channel [0,1]		
+						
+		HSLAPixel();
+		HSLAPixel(double hue, double saturation, double luminance);
+		HSLAPixel(double hue, double saturation, double luminance, double alpha);
+											
+};
 }
